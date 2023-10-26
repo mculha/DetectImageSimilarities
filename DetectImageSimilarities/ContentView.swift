f44f3af8c2722be1b5f0c12007e43ee52da76a14
mculha <melih.culha.09@gmail.com>
2023-10-26 23:56:33 +0300

    Initial Commit

---
+++ DetectImageSimilarities/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DetectImageSimilarities
+//
+//  Created by Melih Çulha on 26.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DetectImageSimilarities/DetectImageSimilaritiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectImageSimilaritiesApp.swift
+//  DetectImageSimilarities
+//
+//  Created by Melih Çulha on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DetectImageSimilaritiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
