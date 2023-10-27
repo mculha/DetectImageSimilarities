7f64ca3fd94546a1564447f389b040e199f4f8e5
mculha <melih.culha.09@gmail.com>
2023-10-27 23:44:11 +0300

    Building up a Permission Utility to Use Later Easily

---
+++ DetectImageSimilarities/Utility/Permission/PermissionProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  PermissionProtocol.swift
+//  DetectImageSimilarities
+//
+//  Created by Melih Çulha on 27.10.2023.
+//
+
+import Foundation
+
+protocol PermissionProtocol {
+    
+    func requestAuthorization() async -> AuthorizationStatus
+    
+}
+
+enum AuthorizationStatus {
+    case authorized
+    case denied
+    case notDetermined
+}
---
+++ DetectImageSimilarities/Utility/Permission/PhotosPermission.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosPermission.swift
+//  DetectImageSimilarities
+//
+//  Created by Melih Çulha on 27.10.2023.
+//
+
+import Foundation
+import Photos
+
+final class PhotosPermission: PermissionProtocol {
+    
+    func requestAuthorization() async -> AuthorizationStatus {
+        let status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        if status == .authorized {
+            return .authorized
+        } else if status == .notDetermined {
+            let status = await PHPhotoLibrary.requestAuthorization(for: .readWrite)
+            return status == .authorized ? .authorized : .denied
+        } else {
+            return .denied
+        }
+    }
+}
