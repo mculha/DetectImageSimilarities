421961ba36e3d7103f00cdc250943f3b1488efc6
mculha <melih.culha.09@gmail.com>
2023-11-09 23:52:16 +0300

    Implementation of DispatchQueue to Manage the Sync Queue

--- DetectImageSimilarities/Screen/Main/PhotoCollectionViewModel.swift
+++ DetectImageSimilarities/Screen/Main/PhotoCollectionViewModel.swift
@@ -76,30 +76,53 @@ import Vision
     }
     
     private func findSimilarities(images: [ImageModel]) {
+        let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
+        let group = DispatchGroup()
+        
         for i in 0..<100 {
             print("Deneme i has been Changed to \(i + 1)")
-
+            
             for j in (i + 1)..<100 {
-                let sourceImage = images[i].image
-                let destinationImage = images[j].image
-                let sourceObservation = observation(for: sourceImage)!
-                let destinationObservation = observation(for: destinationImage)!
-                
-                var distance = Float(0)
-                try? destinationObservation.computeDistance(&distance, to: sourceObservation)
-                if distance < 0.2 {
-                    similarPhotos.append(destinationImage)
-                    similarPhotos.append(sourceImage)
+                concurrentQueue.async(group: group) {
+                    // Your synchronous code goes here
+                    let sourceImage = images[i].image
+                    let destinationImage = images[j].image
+                    let sourceObservation = self.observation(for: sourceImage, i: i, j: j)
+                    let destinationObservation = self.observation(for: destinationImage, i: i, j: j)
+                    
+                    if let sourceObservation, let destinationObservation {
+                        var distance = Float(0)
+                        try? destinationObservation.computeDistance(&distance, to: sourceObservation)
+                        if distance < 0.2 {
+                            self.similarPhotos.append(destinationImage)
+                            self.similarPhotos.append(sourceImage)
+                        }
+                    }
+                    
+                    print("Deneme i: \(i) - j: \(j)")
+                    
                 }
             }
-            
         }
+        group.wait()
+        group.notify(queue: DispatchQueue.main) {
+            print("Deneme All tasks have completed")
+        }
+        
+
     }
     
-    func observation(for image: UIImage) -> VNFeaturePrintObservation? {
+    
+    private func observation(for image: UIImage, i: Int, j: Int) -> VNFeaturePrintObservation? {
         let requestHandler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
         let request = VNGenerateImageFeaturePrintRequest()
-        try? requestHandler.perform([request])
+        DispatchQueue.main.async {
+            
+            print("Deneme Before Perform i: \(i) - j: \(j)")
+            try? requestHandler.perform([request])
+            print("Deneme After Perform i: \(i) - j: \(j)")
+
+        }
         return request.results?.first as? VNFeaturePrintObservation
     }
 }
